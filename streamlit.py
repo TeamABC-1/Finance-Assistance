e2b6e916a4ef15296e7aff19f1f2d50ab5ec9c3f
TeamABC-1 <neeraj6122@gmail.com>
2024-02-16 11:04:19 +0530

    Comment

--- streamlit.py
+++ streamlit.py
@@ -10,26 +10,25 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Function to extract important sentences from a PDF based on keywords
-def extract_important_sentences_from_pdf(pdf_path, keywords):
-    with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        important_sentences = []
+def extract_important_sentences_from_pdf(file, keywords):
+    pdf_reader = PyPDF2.PdfReader(file)
+    important_sentences = []
 
-        for page in pdf_reader.pages:
-            page_text = page.extract_text()
-            sentences = sent_tokenize(page_text)
-            words = word_tokenize(page_text)
-            stop_words = set(stopwords.words('english'))
-            words = [word for word in words if word.lower() not in stop_words]
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        sentences = sent_tokenize(page_text)
+        words = word_tokenize(page_text)
+        stop_words = set(stopwords.words('english'))
+        words = [word for word in words if word.lower() not in stop_words]
 
-            fdist = FreqDist(words)
+        fdist = FreqDist(words)
 
-            for sentence in sentences:
-                if any(keyword.lower() in sentence.lower() for keyword in keywords):
-                    important_sentences.append(sentence)
+        for sentence in sentences:
+            if any(keyword.lower() in sentence.lower() for keyword in keywords):
+                important_sentences.append(sentence)
 
-        important_sentences_text = '\n'.join(important_sentences)
-        return important_sentences_text
+    important_sentences_text = '\n'.join(important_sentences)
+    return important_sentences_text
 
 # Streamlit app
 def main():
